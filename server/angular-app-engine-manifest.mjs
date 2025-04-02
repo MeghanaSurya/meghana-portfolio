53778d54802eb49a7456e4fe5730df62be5ebbcc
Surya Meghana Chochupatla <meghana.chochupatla@gmail.com>
2025-04-02 15:57:38 -0400

    Auto-generated commit

---
+++ server/angular-app-engine-manifest.mjs
@@ -0,0 +1,10 @@
+
+export default {
+  basePath: 'https://MeghanaSurya.github.io/meghana-portfolio',
+  supportedLocales: {
+  "en-US": ""
+},
+  entryPoints: {
+    '': () => import('./main.server.mjs')
+  },
+};
